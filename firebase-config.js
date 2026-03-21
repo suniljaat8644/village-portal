4e76cef72b666be67689a4dc70efa8bf8268bf77
Sunil Choudhary <sunilchoudhry8644@gmail.com>
2026-03-21 21:58:43 +0530

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+// firebase-config.js
+export const firebaseConfig = {
+    apiKey: "AIzaSyD10nmwLn3FjAYsJVcmpm0_removed_for_privacy",
+    authDomain: "village-portal-b4dce.firebaseapp.com",
+    projectId: "village-portal-b4dce",
+    storageBucket: "village-portal-b4dce.firebasestorage.app",
+    messagingSenderId: "233871401426",
+    appId: "1:233871401426:web:e6522e90b90fd0ed9ba80b",
+    measurementId: "G-4X038XL2K3"
+};
